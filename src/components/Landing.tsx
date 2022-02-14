c23b506c0f953ec3f4e2b1a05f537ebd761f2d2e
abhishek.singh <abhishek.singh@arista.com>
2022-02-14 13:29:37 +0000

    Add landing page

---
+++ src/components/Landing.tsx
@@ -0,0 +1,131 @@
+import { createStyles, makeStyles } from '@mui/styles';
+
+import bg from '../assets/bg.png';
+import ghost1 from '../assets/ghost1.png';
+import ghost2 from '../assets/ghost2.png';
+import ghost3 from '../assets/ghost3.png';
+
+export const Landing = () => {
+    const classes = useStyles();
+    return (
+        <section className={classes.banner}>
+            <div className={classes.container}>
+                <img src={bg} className={classes.bg} alt=""/>
+                <div className={classes.grad}></div>
+                <div className={classes.txt}>
+                    <span className={classes.welcome}>Welcome to</span>
+                    <h1 className={classes.title}>Baby Ghosts</h1>
+                </div>
+                <div className={classes.ghosts}>
+                    <div className={classes.ghost1}>
+                        <img src={ghost1} style={{width:'100%', height:'auto'}} alt=""/>
+                    </div>
+                    <div className={classes.ghost2}>
+                        <img src={ghost2} style={{width:'100%', height:'auto'}} alt=""/>
+                    </div>
+                    <div className={classes.ghost1}>
+                        <img src={ghost3} style={{width:'100%', height:'auto'}} alt=""/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        banner: {
+            position: 'relative',
+            padding: '146px 0 57px',
+            zIndex: 1,
+        },
+        container: {
+            padding: '0 20px',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            maxWidth: '1350px',
+            minHeight: '640px',
+            maxHeight: '10000px',
+        },
+        bg:{
+            position: 'absolute',
+            width: '100%',
+            minHeight: '90vh',
+            top: 0,
+            left: 0,
+            right: 0,
+            zIndex: -1,
+        },
+        grad: {
+            position: 'absolute',
+            top: 0,
+            left:0,
+            right:0,
+            bottom: 0,
+            zIndex: 1,
+            height: '100%',
+            background:'linear-gradient(180deg,rgba(0,0,0,0) 0%,black 70%)'
+        },
+        txt: {
+            marginTop: '0px',
+            width: '100%',
+            position: 'absolute',
+            zIndex: 2,
+            textAlign: 'center'
+        },
+        welcome: {
+            color: '#ff6265',
+            fontSize: '12px',
+            marginBottom: '12px',
+            textTransform: 'uppercase',
+            letterSpacing: '2.4px',
+            fontWeight: '700',
+            textAlign: 'center'
+        },
+        title: {
+            fontSize: '90px',
+            marginBottom: '-10px',
+            verticalAlign: 'top',
+            marginTop: '0px',
+            color: '#fff',
+            textTransform: 'uppercase'
+        },
+        ghosts: {
+            marginBottom: '29px',
+            transform:'none',
+            display:'flex',
+            flexWrap:'wrap',
+            alignItems:'flex-end',
+            justifyContent:'center',
+            paddingTop: '50px',
+        },
+        ghost1: {
+            opacity:1,
+            animation:`$dance 4s ease-in-out infinite`,
+            width: '390px',
+            position:'relative',
+            zIndex:3,
+            transition:'opacity .5s ease-in-out,transform .5s ease-in-out,-webkit-transform .5s ease-in-out',
+        },
+        ghost2: {
+            opacity:1,
+            animation:`$dance 4s ease-in-out infinite`,
+            width: '462px',
+            position:'relative',
+            zIndex:4,
+            margin:'0 -294px 0 -274px',
+            transition:'opacity .5s ease-in-out,transform .5s ease-in-out,-webkit-transform .5s ease-in-out',
+        },
+        "@keyframes dance": {
+            "0%": {
+              transform: "translatey(0px)"
+            },
+            "50%": {
+                transform: "translatey(-20px)"
+            },
+            "100%": {
+              transform: "translatey(0px)"
+            }
+          },
+    })
+)
---
+++ src/pages/Home.tsx
@@ -0,0 +1,115 @@
+import { createStyles, makeStyles } from '@mui/styles';
+
+import { Landing } from "../components/Landing";
+import logo from '../assets/logo.png';
+import discord from '../assets/logo/discord.svg';
+import twitter from '../assets/logo/twitter.svg';
+import opensea from '../assets/logo/opensea.svg';
+
+export const Home = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.wrapper}>
+            <div className={classes.navbar}>
+                <div className={classes.container}>
+                    <img src={logo} className={classes.logo} alt="logo"/>
+                    <div className={classes.nav}>
+                        <ul className={classes.menulist}>
+                            <li className={classes.menuitem}>Welcome</li>
+                            <li className={classes.menuitem}>Goals</li>
+                            <li className={classes.menuitem}>Rarity</li>
+                            <li className={classes.menuitem}>Team</li>
+                            <li className={classes.menuitem}>Faq</li>
+                        </ul>
+                        <ul className={classes.sociallist}>
+                            <li className={classes.socialitem}>
+                                <img src={opensea} className={classes.socialicon} alt="opensea"/>
+                            </li>
+                            <li className={classes.socialitem}>
+                                <img src={twitter} className={classes.socialicon} alt="twitter"/>
+                            </li>
+                            <li className={classes.socialitem}>
+                                <img src={discord} className={classes.socialicon} alt="discord"/>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div style={{display:'block'}}>
+                <Landing />
+            </div>
+            {/* Main Page */}
+        </div>
+    )
+}
+
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        wrapper: {
+            overflow: 'hidden',
+            position: 'relative',
+            maxHeight: '100000px',
+            color: '#989898'
+        },
+        navbar: {
+            position: 'absolute',
+            padding: '30px 0 30px 0',
+            left: 0,
+            right: 0,
+            top: 0,
+            width: '100%',
+            zIndex: '999'
+        },
+        container:{
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+            padding: '0 30px 0 30px',
+            maxWidth: '1350px',
+            margin: '0 45px',
+        },
+        logo: {
+            width: '67px',
+            height: 'auto',
+        },
+        nav: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+            width: 'calc(100% - 67px)'
+        },
+        menulist: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            fontSize: '11px',
+            fontWeight: '700',
+            fontFamily: 'inter',
+            paddingLeft: '38px',
+            width: '80%',
+            textTransform:'uppercase'
+        },
+        menuitem: {
+            marginRight: '30px',
+            listStyle: 'none',
+            cursor: 'pointer',
+            letterSpacing: '1.2px'
+        },
+        sociallist: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            marginRight: '-10px'
+        },
+        socialitem: {
+            marginRight: '10px',
+            listStyle: 'none',
+        },
+        socialicon: {
+            border: 'none',
+            verticalAlign: 'top',
+            maxWidth: '100%',
+            height: 'auto'
+        }
+    })
+)
